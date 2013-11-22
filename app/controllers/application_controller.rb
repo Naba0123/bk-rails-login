2f80bfceeb85ae5af09a4bc033ba2c0a969727d1
Naba16 <mikumiku_dtm@yahoo.co.jp>
2013-11-22 22:00:15 +0900

    一部保存されてなかった・・・

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,11 @@ class ApplicationController < ActionController::Base
   
   private
   
-  def current_user  
-    @current_user ||= User.find(session[:user_id])
-  end
+#  def current_user  
+#    @current_user ||= User.find(session[:user_id])
+#  end
     
-  helper_method :current_user
+#  helper_method :current_user
   
   def check_logined
     if session[:user_id]
